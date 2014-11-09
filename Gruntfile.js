d9baa0f551a632096dfba6c89b939a4072015bfb
Thomas Ritter <ritter.thomas@gmail.com>
2014-11-09 22:45:55 +0100

    Disable live reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         open: true,
-        livereload: 35729,
+        // livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },
